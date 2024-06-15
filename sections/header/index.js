6e02eccf63e961b17b2cf44bf59486afb7bed1d3
vahe poghosyan <vahikpoghosyan@gmail.com>
2024-06-15 11:44:43 +0400

    first commit

---
+++ sections/header/index.js
@@ -0,0 +1,130 @@
+export const header = `
+        <div>
+            <h1 class="fw-semibold">Vahe Poghosyan</h1>
+            <h5 class="fw-normal text-muted mb-4">Senior Software Engineer</h5>
+
+            <div class="d-flex">
+            <a
+                href="https://www.google.com/maps/place/armenia+yerevan/data=!4m2!3m1!1s0x406aa2dab8fc8b5b:0x3d1479ae87da526a?sa=X&ved=1t:155783&ictx=111"
+                class="link-secondary text-decoration-none d-flex me-2"
+                target="_blank"
+              >
+                <img
+                  src="./assets/images/icons/geo-alt-fill.svg"
+                  alt="compass"
+                  class="me-2 icons"
+                />
+              </a>
+              <a
+                href="mailto:vahe.inbox@gmail.com"
+                class="link-secondary text-decoration-none d-flex me-2"
+              >
+                <img
+                  src="./assets/images/icons/envelope.svg"
+                  alt="envelope"
+                  class="me-2 icons"
+                />
+              </a>
+              
+              <a
+                href="tel:+37495464229"
+                class="link-secondary text-decoration-none d-flex me-2"
+              >
+                <img
+                  src="./assets/images/icons/telephone.svg"
+                  alt="telephone"
+                  class="me-2 icons"
+                />
+              </a>
+              <a
+                href="https://github.com/vahepoghosyan"
+                class="link-secondary text-decoration-none d-flex me-2"
+                target="_blank"
+              >
+                <img
+                  src="./assets/images/icons/github.svg"
+                  alt="github"
+                  class="me-2 icons"
+                />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/vahe-poghosyan-7bb70aa0/"
+                class="link-secondary text-decoration-none d-flex me-2"
+                target="_blank"
+              >
+                <img
+                  src="./assets/images/icons/linkedin.svg"
+                  alt="linkedin"
+                  class="me-2 icons"
+                />
+              </a>
+            </div>
+      </div>
+      <div class="w-25">
+        <img
+          src="./assets/images/avatar.jpg"
+          alt="profile"
+          class="rounded-circle w-100"
+        />
+      </div>
+`;
+
+// <div className="d-flex flex-column">
+//     <a
+//         href="mailto:vahe.inbox@gmail.com"
+//         className="link-secondary text-decoration-none d-flex"
+//     >
+//         <img
+//             src="./assets/images/icons/envelope.svg"
+//             alt="envelope"
+//             className="me-1"
+//         />
+//         vahe.inbox@gmail.com
+//     </a>
+//     <a
+//         href="mailto:vahe.inbox@gmail.com"
+//         className="link-secondary text-decoration-none d-flex"
+//     >
+//         <img
+//             src="./assets/images/icons/compass.svg"
+//             alt="compass"
+//             className="me-1"
+//         />
+//         Yerevan, Armenia GMT+4
+//     </a>
+//     <a
+//         href="mailto:vahe.inbox@gmail.com"
+//         className="link-secondary text-decoration-none d-flex"
+//     >
+//         <img
+//             src="./assets/images/icons/telephone.svg"
+//             alt="telephone"
+//             className="me-1"
+//         />
+//         +374 95 464 229
+//     </a>
+//     <a
+//         href="https://github.com/vahepoghosyan"
+//         className="link-secondary text-decoration-none d-flex"
+//         target="_blank"
+//     >
+//         <img
+//             src="./assets/images/icons/github.svg"
+//             alt="github"
+//             className="me-1"
+//         />
+//         vahepoghosyan
+//     </a>
+//     <a
+//         href="https://www.linkedin.com/in/vahe-poghosyan-7bb70aa0/"
+//         className="link-secondary text-decoration-none d-flex"
+//         target="_blank"
+//     >
+//         <img
+//             src="./assets/images/icons/linkedin.svg"
+//             alt="linkedin"
+//             className="me-1"
+//         />
+//         Vahe Poghosyan
+//     </a>
+// </div>
